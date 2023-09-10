138ba351b3587b61061c77ede2e5b8bca2758cc8
Anton <anton.batalov94@gmail.com>
2023-09-10 18:53:08 +0700

    Task 5.5.5 and 5.5.8 done

---
+++ Task_5_5_5/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_5_5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine(Factorial(x));
+            Console.WriteLine(PowerUp(x, 3));
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        //task 5.5.8
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
+
+    }
+}
